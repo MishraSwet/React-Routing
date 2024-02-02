1fac7a8de89c609055a43fed1e3eef39c5f70e0b
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-02-02 23:55:41 +0530

    Added Client Routing using Window location

--- React-Routing/src/App.jsx
+++ React-Routing/src/App.jsx
@@ -1,9 +1,13 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import { Dashboard } from "./Components/Dashboard"
 import { Landing } from "./Components/Landing"
-export function App() {
+export default function App() {
   return (
     <>
+      <div>
+        <button onClick={() => { window.location.href = "/" }}>Go Home </button>
+        <button onClick={()=>{window.location.href="/Dashboard"}}>Go DashBoard</button>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={Landing} />
